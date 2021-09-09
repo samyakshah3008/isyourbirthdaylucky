e63dfc817b581952acadca2d97d6eeb8536b476d
Samyak Shah <samyakshah3008@gmail.com>
2021-09-09 20:50:29 +0530

    FIX: negative and null value error show

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ const outputBox = document.querySelector("#output-box")
 
 alert("Please note that we are not storing your data anywhere")
 
+
+
 function compareValues(sum,luckyNumber){
     if(sum%luckyNumber===0){
         outputBox.innerText = "Yay,you are lucky!" 
 
-    }
+    } 
     else{
         outputBox.innerText="You are not that lucky!"
         
@@ -25,10 +27,22 @@ function compareValues(sum,luckyNumber){
 function checkBirthDateIsLucky(){
     const dob = dateOfBirth.value;
     const sum = calculateSum(dob);
-    compareValues(sum,luckyNumber.value)
+    
+    if(luckyNumber.value === ""){
+        outputBox.innerText = "Fill the lucky number so we can calculate"
+    } else if(luckyNumber.value < 0){
+        outputBox.innerText = "Negative values not allowed"
+    } else if(dateOfBirth.value === ""){
+        outputBox.innerText = "Fill your date of birth so we can calculate"
+    } 
+    
+    else{
+        compareValues(sum,luckyNumber.value)
+    }
+
 }
 
-checkNumberButton.addEventListener('click',checkBirthDateIsLucky) 
+
 
 
 function calculateSum(dob) {
@@ -40,6 +54,4 @@ function calculateSum(dob) {
    return sum;
 }
 
-checkNumberButton.addEventListener('click',function getValues(){
-    console.log(checkBirthDateIsLucky())
-})
+checkNumberButton.addEventListener('click', checkBirthDateIsLucky)
